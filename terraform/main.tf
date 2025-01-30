d1052db5c097488e750ad43b919923bd333d5253
David Cardoso <david.lima.cd@gmail.com>
2025-01-30 13:21:26 -0300

    33

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 resource "null_resource" "default" {
   provisioner "local-exec" {
-    command = "echo 'Hello World 2'"
+    command = "echo 'Hello World 3'"
   }
 }
 
 output "test" {
-  value = "Teste 2"
+  value = "Teste 3"
 } 
 ##
