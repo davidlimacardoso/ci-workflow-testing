3e5bb96b27c7affb11033f74ed15b5420a7c5ee8
David Cardoso <david.lima.cd@gmail.com>
2025-01-31 08:53:27 -0300

    Revert "2222"

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 resource "null_resource" "default" {
   provisioner "local-exec" {
-    command = "echo 'Hello World 2'"
+    command = "echo 'Hello World 1'"
   }
 }
 
 output "test" {
-  value = "Teste 2"
+  value = "Teste 1"
 } 
 ##
